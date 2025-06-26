21d46013b7e40a6e55f0efd7e673929f2f98581f
Filipokinson <cvalukivan@gmail.com>
2025-06-26 13:36:27 +0600

    feat: add method-extension realisation

---
+++ task01/Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+public static class StringExtensions
+{
+    public static bool IsPalindrome(this string input)
+    {
+        if (string.IsNullOrEmpty(input))
+        {
+            return false;
+        }
+        string cleaninput = new string(input.ToLower().Where(c => !char.IsWhiteSpace(c) && !char.IsPunctuation(c)).ToArray());
+
+        if (string.IsNullOrEmpty(cleaninput))
+        {
+            return false;
+        }
+        string reverse = new string(cleaninput.Reverse().ToArray());
+
+        return reverse == cleaninput;
+    }
+}
