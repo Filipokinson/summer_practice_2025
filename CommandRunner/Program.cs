d2f65a808d3ee275ad58efa4c186f5b1bbadb275
Filipokinson <cvalukivan@gmail.com>
2025-07-05 23:58:54 +0600

    feat: add dynamic libraries realisation

---
+++ CommandRunner/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+
+class Program
+{
+    static void Main()
+    {
+        string path = Path.Combine(Directory.GetCurrentDirectory(), "TestDir");
+
+        Assembly assembly = Assembly.LoadFrom("../FileSystemCommands/bin/Debug/net9.0/FileSystemCommands.dll");
+
+        var dszType = assembly.GetType("FileSystemCommands.DirectorySizeCommand");
+        var dszInstance = (ICommand)Activator.CreateInstance(dszType, path);
+        dszInstance.Execute();
+
+        var ffcType = assembly.GetType("FileSystemCommands.FindFilesCommand");
+        var ffcInstance = (ICommand)Activator.CreateInstance(ffcType, path);
+        ffcInstance.Execute();
+    }
+}
