831a910516fb22f1d09ac9bb43b13fddc5d96a66
Filipokinson <cvalukivan@gmail.com>
2025-07-18 01:23:59 +0600

    feat: improve calculator

--- task14/Class1.cs
+++ task14/Class1.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using System.Threading;
+using System.Threading.Tasks;
 
 public class DefiniteIntegral
 {
@@ -12,40 +12,56 @@ public class DefiniteIntegral
         int stepsPerThread = totalSteps / threadsNumber;
         int remainder = totalSteps % threadsNumber;
 
-        Thread[] threads = new Thread[threadsNumber];
-
-        for (int i = 0; i < threadsNumber; i++)
+        Parallel.For(0, threadsNumber, new ParallelOptions { MaxDegreeOfParallelism = threadsNumber }, threadIdx =>
         {
-            int threadIdx = i;
-            threads[i] = new Thread(() =>
+            int startIdx = threadIdx * stepsPerThread + Math.Min(threadIdx, remainder);
+            int count = stepsPerThread + (threadIdx < remainder ? 1 : 0);
+            double localA = a + startIdx * step;
+            double localB = localA + count * step;
+            if (localB > b) localB = b;
+
+            double localResult = 0.0;
+            double x0 = localA;
+            for (int j = 0; j < count; j++)
             {
-                int startIdx = threadIdx * stepsPerThread + Math.Min(threadIdx, remainder);
-                int count = stepsPerThread + (threadIdx < remainder ? 1 : 0);
-                double localA = a + startIdx * step;
-                double localB = localA + count * step;
-                if (localB > b) localB = b;
+                double x1 = x0 + step;
+                if (x1 > b) x1 = b;
+                double f0 = function(x0);
+                double f1 = function(x1);
+                localResult += 0.5 * (f0 + f1) * (x1 - x0);
+                x0 = x1;
+            }
 
-                double localResult = 0.0;
-                double x0 = localA;
-                for (int j = 0; j < count; j++)
-                {
-                    double x1 = x0 + step;
-                    if (x1 > b) x1 = b;
-                    double f0 = function(x0);
-                    double f1 = function(x1);
-                    localResult += 0.5 * (f0 + f1) * (x1 - x0);
-                    x0 = x1;
-                }
+            lock (locker)
+            {
+                result += localResult;
+            }
+        });
 
-                lock (locker)
-                {
-                    result += localResult;
-                }
-            });
-            threads[i].Start();
+        return result;
+    }
+}
+
+public class DefiniteIntegralSingleThread
+{
+    public static double SolveSimple(double a, double b, Func<double, double> function, double step)
+    {
+        double result = 0.0;
+        double x = a;
+
+        while (x + step < b)
+        {
+            double f0 = function(x);
+            double f1 = function(x + step);
+            result += 0.5 * (f0 + f1) * step;
+            x += step;
         }
-
-        foreach (var t in threads) t.Join();
+        if (x < b)
+        {
+            double f0 = function(x);
+            double f1 = function(b);
+            result += 0.5 * (f0 + f1) * (b - x);
+        }
 
         return result;
     }
